db44118691db23d3a98c7a2d0e079b4f59f8cb19
Bernardo Pinho <bernardopinho@gmail.com>
2017-10-03 19:49:51 +0100

    Core functionality established
    (Currently hard-coded to read a test.xml on the same directory as the exe)

--- TexturePackerSpritesheetToAchx/Program.cs
+++ TexturePackerSpritesheetToAchx/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace TexturePackerSpritesheetToAchx
 {
@@ -10,6 +11,35 @@ namespace TexturePackerSpritesheetToAchx
     {
         static void Main(string[] args)
         {
+            XDocument TexturePackerXml = XDocument.Load("test.xml");
+
+            XElement frameListParent = TexturePackerXml.Root.Element("AnimationChain");
+
+            foreach (var frame in frameListParent.Elements("Frame"))
+            {
+                var xNode = frame.Element("X");
+                var yNode = frame.Element("Y");
+                var widthNode = frame.Element("Width");
+                var heightNode = frame.Element("Height");
+
+                int x = int.Parse(xNode.Value);
+                int y = int.Parse(yNode.Value);
+                int width =  int.Parse(widthNode.Value);
+                int height = int.Parse(heightNode.Value);
+
+                frame.Add(new XElement("LeftCoordinate", x));
+                frame.Add(new XElement("RightCoordinate", x + width));
+                frame.Add(new XElement("TopCoordinate", y));
+                frame.Add(new XElement("BottomCoordinate", y + height));
+                
+
+                xNode.Remove();
+                yNode.Remove();
+                widthNode.Remove();
+                heightNode.Remove();
+            }
+
+            TexturePackerXml.Save("output.xml");
         }
     }
 }
